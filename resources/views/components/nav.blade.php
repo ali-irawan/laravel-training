c241c5f82e88fadfc594061e67606762977feafe
Ali Irawan <boylevantz@gmail.com>
2019-05-11 13:00:30 +0700

    add laravelmix and css

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,5 +1,6 @@
 <nav class="nav">
-            <a class="nav-link" href="{!! route('home') !!}">Home</a>
-            <a class="nav-link" href="{!! route('about') !!}">About</a>
-            <a class="nav-link" href="{!! route('contact') !!}">Contact Us</a>
-        </nav>
+            <a class="nav-link {{ (Route::currentRouteName()=='home') ? 'active' : ''  }}" href="{!! route('home') !!}">Home</a>
+            <a class="nav-link {{ (Route::currentRouteName()=='about') ? 'active' : ''  }}" href="{!! route('about') !!}">About</a>
+            <a class="nav-link {{ (Route::currentRouteName()=='contact') ? 'active' : ''  }}" href="{!! route('contact') !!}">Contact Us</a>
+</nav>
+
