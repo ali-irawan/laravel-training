735fdefd28943be3afee154a0e1f918f31ff45f8
Ali Irawan <boylevantz@gmail.com>
2019-05-25 11:21:53 +0700

    contact form route test

--- tests/Unit/ContactFormRouteTest.php
+++ tests/Unit/ContactFormRouteTest.php
@@ -18,14 +18,26 @@ class ContactFormRouteTest extends TestCase
      */
     public function testExample()
     {
+        $before_count = \App\FormContact::count();
+        $this->assertEquals( 9, $before_count );
+
         $response = $this->post('/contact', 
         [
-            'email' => 'boylevantz@gmail.com',
-            'topic' => 'Technical',
-            'message' => 'testing testing',
+            'email' => 'boylevantz@yahoo.com',
+            'topic' => 'Billing',
+            'message' => 'testing again',
         ]
         );
 
         $response->assertStatus(302); // redirected
+
+        $after_count = \App\FormContact::count();   
+        $this->assertEquals( 10, $after_count);
+
+        $last_data = \App\FormContact::orderBy('id', 'desc')->first();
+
+        $this->assertEquals('boylevantz@yahoo.com', $last_data->email);
+        $this->assertEquals('Billing', $last_data->topic);
+        $this->assertEquals('testing again', $last_data->message);
     }
 }
